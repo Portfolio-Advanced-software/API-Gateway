dcdea3408e0c7781de6adb7714eddeec4417294b
Milofow <Milofow@hotmail.nl>
2023-06-03 12:59:15 +0200

    ready for protogen

---
+++ pkg/authz/client.go
@@ -0,0 +1,24 @@
+package authz
+
+import (
+	"fmt"
+
+	"github.com/Portfolio-Advanced-software/API-Gateway/pkg/authz/pb"
+	"github.com/Portfolio-Advanced-software/API-Gateway/pkg/config"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.AuthServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.AuthServiceClient {
+	// using WithInsecure() because no SSL running
+	cc, err := grpc.Dial(c.AuthSvcUrl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	return pb.NewAuthServiceClient(cc)
+}
